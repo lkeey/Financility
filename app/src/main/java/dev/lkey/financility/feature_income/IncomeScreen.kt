284b544781750e51c29b892fc08defe80499bba8
Aleksey Kiryushin <97094952+lkeey@users.noreply.github.com>
2025-06-09 12:42:18 +0300

    create income screen

--- app/src/main/java/dev/lkey/financility/feature_income/IncomeScreen.kt
+++ app/src/main/java/dev/lkey/financility/feature_income/IncomeScreen.kt
@@ -1,11 +1,71 @@
 package dev.lkey.financility.feature_income
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import dev.lkey.financility.R
+import dev.lkey.financility.components.FinancilityBottomBar
+import dev.lkey.financility.components.FinancilityTopBar
+import dev.lkey.financility.feature_expenses.presentation.ExpensesView
 
 @Composable
 fun IncomeScreen (
     navController: NavController
 ) {
-    IncomeView()
+    Scaffold (
+        bottomBar = {
+            FinancilityBottomBar(
+                navController = navController
+            )
+        },
+        topBar = {
+            FinancilityTopBar(
+                title = "Доходы сегодня",
+                icon = {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_history),
+                        contentDescription = "История",
+                        tint = MaterialTheme.colorScheme.surfaceContainer
+                    )
+                }
+            )
+        },
+        modifier = Modifier
+            .fillMaxSize(),
+        containerColor = MaterialTheme.colorScheme.onSurface,
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /* TODO */ },
+                shape = CircleShape,
+                containerColor = MaterialTheme.colorScheme.primary
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.ic_plus),
+                    contentDescription = "add button",
+                    modifier = Modifier
+                        .size(16.dp),
+                    tint = Color.White
+                )
+            }
+        },
+        floatingActionButtonPosition = FabPosition.End
+    ) { padding ->
+
+        IncomeView(
+            modifier = Modifier.padding(padding)
+        )
+
+    }
 }
--- app/src/main/java/dev/lkey/financility/feature_income/IncomeView.kt
+++ app/src/main/java/dev/lkey/financility/feature_income/IncomeView.kt
@@ -1,10 +1,48 @@
 package dev.lkey.financility.feature_income
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import dev.lkey.financility.R
+import dev.lkey.financility.components.ListItem
+import dev.lkey.financility.feature_expenses.data.MockTransactionModel
 
 @Composable
 fun IncomeView (
-
+    modifier: Modifier = Modifier
 ) {
+    val mockData = listOf(
+        MockTransactionModel(
+            title = "Зарплата",
+            cost = "500 000 ₽"
+        ),
+        MockTransactionModel(
+            title = "Подработка",
+            cost = "100 000 ₽"
+        ),
+    )
 
+    Column (
+        modifier = modifier
+            .fillMaxSize()
+    ){
+        ListItem(
+            title = "Всего",
+            description = null,
+            backgroundColor = MaterialTheme.colorScheme.surfaceContainerLow,
+            trailingText = "6000 000 ₽"
+        )
+
+        mockData.forEach {
+            ListItem(
+                trailingIcon = R.drawable.ic_light_arrow,
+                emoji = it.emoji,
+                title = it.title,
+                description = it.description,
+                trailingText = it.cost
+            )
+        }
+    }
 }
