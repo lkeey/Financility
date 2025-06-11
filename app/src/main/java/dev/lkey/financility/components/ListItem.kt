28cbfb892972e4b86981fbce3b86d729da9cd198
Aleksey Kiryushin <97094952+lkeey@users.noreply.github.com>
2025-06-11 19:08:47 +0300

    create mock repository

--- app/src/main/java/dev/lkey/financility/components/ListItem.kt
+++ app/src/main/java/dev/lkey/financility/components/ListItem.kt
@@ -20,14 +20,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-
 
 
 @Composable
