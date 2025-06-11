cc580b0598c10769cd220fd07776a9aa32b9c9ae
Aleksey Kiryushin <97094952+lkeey@users.noreply.github.com>
2025-06-11 13:08:52 +0300

    create bill screen

--- app/src/main/java/dev/lkey/financility/feature_bill/BillScreen.kt
+++ app/src/main/java/dev/lkey/financility/feature_bill/BillScreen.kt
@@ -1,11 +1,76 @@
 package dev.lkey.financility.feature_bill
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import dev.lkey.financility.R
+import dev.lkey.financility.components.FinancilityBottomBar
+import dev.lkey.financility.components.FinancilityTopBar
+import dev.lkey.financility.feature_expenses.presentation.ExpensesView
 
 @Composable
 fun BillScreen (
     navController: NavController
 ) {
-    BillView()
+    Scaffold (
+        bottomBar = {
+            FinancilityBottomBar(
+                navController = navController
+            )
+        },
+        topBar = {
+            FinancilityTopBar(
+                title = "Мой счет",
+                icon = {
+                    IconButton(
+                        onClick = { /* TODO */ }
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_edit),
+                            contentDescription = "изменить",
+                            tint = MaterialTheme.colorScheme.surfaceContainer
+                        )
+                    }
+                }
+            )
+        },
+        modifier = Modifier
+            .fillMaxSize(),
+        containerColor = MaterialTheme.colorScheme.onSurface,
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /* TODO */ },
+                shape = CircleShape,
+                containerColor = MaterialTheme.colorScheme.primary
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.ic_plus),
+                    contentDescription = "add button",
+                    modifier = Modifier
+                        .size(16.dp),
+                    tint = Color.White
+                )
+            }
+        },
+        floatingActionButtonPosition = FabPosition.End
+    ) { padding ->
+
+        BillView(
+            modifier = Modifier.padding(padding)
+        )
+
+    }
 }
--- app/src/main/java/dev/lkey/financility/feature_bill/BillView.kt
+++ app/src/main/java/dev/lkey/financility/feature_bill/BillView.kt
@@ -1,11 +1,36 @@
 package dev.lkey.financility.feature_bill
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import dev.lkey.financility.R
+import dev.lkey.financility.components.ListItem
 
 @Composable
 fun BillView (
-
+    modifier: Modifier = Modifier
 ) {
-    Text("BILL")
+    Column (
+        modifier = modifier
+            .fillMaxSize()
+    ){
+        ListItem(
+            emoji = "\uD83D\uDC57",
+            title = "Баланс",
+            description = null,
+            backgroundColor = MaterialTheme.colorScheme.surfaceContainerLow,
+            trailingText = "-670 000 ₽",
+            trailingIcon = R.drawable.ic_light_arrow,
+        )
+
+        ListItem(
+            title = "Всего",
+            description = null,
+            backgroundColor = MaterialTheme.colorScheme.surfaceContainerLow,
+            trailingText = "₽",
+            trailingIcon = R.drawable.ic_light_arrow,
+        )
+    }
 }
