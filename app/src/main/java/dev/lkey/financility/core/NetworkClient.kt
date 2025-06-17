4cb0c88f15ecac4bc999561ccd20ebad7fc4371c
Aleksey Kiryushin <97094952+lkeey@users.noreply.github.com>
2025-06-17 17:06:16 +0300

    show articles

--- app/src/main/java/dev/lkey/financility/core/NetworkClient.kt
+++ app/src/main/java/dev/lkey/financility/core/NetworkClient.kt
@@ -1,5 +1,6 @@
 package dev.lkey.financility.core
 
+import dev.lkey.financility.BuildConfig
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.HttpTimeout
@@ -8,6 +9,7 @@ import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.request.header
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
@@ -34,5 +36,6 @@ val ktorClient = HttpClient(OkHttp) {
     }
     defaultRequest {
         contentType(ContentType.Application.Json)
+        header("Authorization", "Bearer ${BuildConfig.API_KEY}")
     }
 }
--- app/src/main/java/dev/lkey/financility/feature_articles/data/api/ArticleRepositoryImpl.kt
+++ app/src/main/java/dev/lkey/financility/feature_articles/data/api/ArticleRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package dev.lkey.financility.feature_articles.data.api
 
+import dev.lkey.financility.BuildConfig
 import dev.lkey.financility.core.ApiException
 import dev.lkey.financility.core.ktorClient
 import dev.lkey.financility.core.safeCall
@@ -15,63 +16,14 @@ class ArticleRepositoryImpl : ArticlesRepository {
     override suspend fun getArticles(): Result<List<CategoryModel>> {
 
         return safeCall {
-            val response: HttpResponse = ktorClient.get("https://shmr-finance.ru/categories")
+            val response: HttpResponse = ktorClient.get("${BuildConfig.BASE_URL}/categories")
 
             if (response.status != HttpStatusCode.OK) {
                 throw ApiException("Ошибка API: ${response.status}")
             }
+
             response.body()
         }
-//        return listOf(
-//            CategoryModel(
-//                id = 0,
-//                name = "Аренда квартиры",
-//                emoji = "\uD83C\uDFE1",
-//                isIncome = true
-//            ),
-//            CategoryModel(
-//                id = 0,
-//                name = "Одежда",
-//                emoji = "\uD83D\uDC57",
-//                isIncome = true
-//            ),
-//            CategoryModel(
-//                id = 0,
-//                name = "На собачку",
-//                emoji = "\uD83D\uDC36",
-//                isIncome = true
-//            ),
-//            CategoryModel(
-//                id = 0,
-//                name = "На собачку",
-//                emoji = "\uD83D\uDC36",
-//                isIncome = true
-//            ),
-//            CategoryModel(
-//                id = 0,
-//                name = "Ремонт квартиры",
-//                emoji = "РК",
-//                isIncome = true
-//            ),
-//            CategoryModel(
-//                id = 0,
-//                name = "Продукты",
-//                emoji = "\uD83C\uDF6D",
-//                isIncome = true
-//            ),
-//            CategoryModel(
-//                id = 0,
-//                name = "Спортзал",
-//                emoji = "\uD83C\uDFCB\uFE0F",
-//                isIncome = true
-//            ),
-//            CategoryModel(
-//                id = 0,
-//                name = "Медицина",
-//                emoji = "\uD83D\uDC8A",
-//                isIncome = true
-//            ),
-//        )
     }
 
 }
